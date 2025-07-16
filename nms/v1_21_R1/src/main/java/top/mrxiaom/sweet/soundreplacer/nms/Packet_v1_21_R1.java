3f1baac7b4307d8f37484f08ea0a908453f55522
MrXiaoM <coolxiaom95@gmail.com>
2025-07-16 14:54:59 +0800

    support 1.21.x

---
+++ nms/v1_21_R1/src/main/java/top/mrxiaom/sweet/soundreplacer/nms/Packet_v1_21_R1.java
@@ -0,0 +1,58 @@
+package top.mrxiaom.sweet.soundreplacer.nms;
+
+import com.comphenix.protocol.events.PacketContainer;
+import net.minecraft.core.Holder;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.protocol.game.PacketPlayOutEntitySound;
+import net.minecraft.network.protocol.game.PacketPlayOutNamedSoundEffect;
+import net.minecraft.network.syncher.DataWatcher;
+import net.minecraft.sounds.SoundEffect;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityTypes;
+import org.bukkit.NamespacedKey;
+import org.bukkit.craftbukkit.v1_21_R1.util.CraftNamespacedKey;
+import org.jetbrains.annotations.Nullable;
+
+public class Packet_v1_21_R1 implements PacketAPI {
+    @Override
+    public @Nullable NamespacedKey getPacketSoundEffect(PacketContainer packet) {
+        Object handle = packet.getHandle();
+        if (handle instanceof PacketPlayOutNamedSoundEffect p) {
+            return CraftNamespacedKey.fromMinecraft(p.b().a().a());
+        }
+        if (handle instanceof PacketPlayOutEntitySound p) {
+            return CraftNamespacedKey.fromMinecraft(p.b().a().a());
+        }
+        return null;
+    }
+
+    @Override
+    public @Nullable Object overrideSound(PacketContainer packet, NamespacedKey sound) {
+        Object handle = packet.getHandle();
+        Holder<SoundEffect> holder = Holder.a(SoundEffect.a(CraftNamespacedKey.toMinecraft(sound)));
+        if (handle instanceof PacketPlayOutNamedSoundEffect p) {
+            return new PacketPlayOutNamedSoundEffect(holder, p.e(), p.f(), p.g(), p.h(), p.i(), p.j(), p.k());
+        }
+        if (handle instanceof PacketPlayOutEntitySound p) {
+            return new PacketPlayOutEntitySound(holder, p.e(), new DummyEntity(p.f()), p.g(), p.h(), p.i());
+        }
+        return null;
+    }
+
+    public static class DummyEntity extends Entity {
+        public DummyEntity(int id) {
+            super(EntityTypes.aD, null);
+            e(id);
+        }
+
+        @Override
+        protected void a(DataWatcher.a a) {
+        }
+        @Override
+        protected void a(NBTTagCompound nbtTagCompound) {
+        }
+        @Override
+        protected void b(NBTTagCompound nbtTagCompound) {
+        }
+    }
+}
---
+++ nms/v1_21_R5/build.gradle.kts
@@ -0,0 +1,11 @@
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.7-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.21.7")
+    compileOnly("com.mojang:datafixerupper:8.0.16")
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    if (JavaVersion.current().isJava10Compatible) {
+        options.release.set(17)
+    }
+}
